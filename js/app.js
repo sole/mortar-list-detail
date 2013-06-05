f9198354e4fe15efe7878449a1f2bb9c4171ede5
Soledad Penades <sole@mozilla.com>
2013-06-05 16:46:32 +0100

    Implemented basic list/detail functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
-// TMP
+
 var slidebox = document.getElementById('slidebox');
 var detail = document.getElementById('detail');
+var detailH1 = detail.querySelector('h1');
+var detailContents = detail.querySelector('.contents');
 
-document.getElementById('list').addEventListener('click', function() {
-    console.log('clicked on', this);
-    slidebox.slideTo(1);
+var articles = document.querySelectorAll('#list article');
+for(var i = 0; i < articles.length; i++) {
+    var article = articles[i];
+    article.addEventListener('click', showDetail, false);
+}
+
+document.querySelector('#detail button').addEventListener('click', function() {
+    slidebox.slideTo(0);
 }, false);
+
+function showDetail(event) {
+    var article = this;
+    var h1 = article.querySelector('h1');
+    var contents = article.querySelector('.contents');
+    
+    detailH1.innerHTML = h1.innerHTML;
+    detailContents.innerHTML = contents.innerHTML;
+
+    slidebox.slideTo(1);
+}
