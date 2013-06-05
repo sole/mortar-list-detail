a9f847e2eaf9414d2aa63a115735333ae2bc0993
Soledad Penades <sole@mozilla.com>
2013-06-05 18:42:04 +0100

    Add install button. Also, change indentation on the html file to 2 spaces.

--- js/app.js
+++ js/app.js
@@ -4,15 +4,43 @@ var detail = document.getElementById('detail');
 var detailH1 = detail.querySelector('h1');
 var detailContents = detail.querySelector('.contents');
 
-var articles = document.querySelectorAll('#list article');
-for(var i = 0; i < articles.length; i++) {
-    var article = articles[i];
-    article.addEventListener('click', showDetail, false);
-}
+function init() {
 
-document.querySelector('#detail button').addEventListener('click', function() {
-    slidebox.slideTo(0);
-}, false);
+    // Set up event listeners for reacting to mouse / touch actions
+
+    var articles = document.querySelectorAll('#list article');
+    for(var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        article.addEventListener('click', showDetail, false);
+    }
+
+    document.querySelector('#detail button').addEventListener('click', function() {
+        slidebox.slideTo(0);
+    }, false);
+
+
+    // This code shows an install button if the apps platform is available
+    // and this app isn't already installed.
+    var installButton = document.getElementById('install-btn');
+
+    installButton.style.display = 'none';
+
+    if(navigator.mozApps) {
+
+        installButton.addEventListener('click', function() {
+            navigator.mozApps.install(location.href + 'manifest.webapp');
+        }, false);
+
+        var req = navigator.mozApps.getSelf();
+        req.onsuccess = function() {
+            if(!req.result) {
+                installButton.style.display = 'block';
+            }
+        };
+
+    }
+
+}
 
 function showDetail(event) {
     var article = this;
@@ -24,3 +52,8 @@ function showDetail(event) {
 
     slidebox.slideTo(1);
 }
+
+// ---
+
+init();
+
